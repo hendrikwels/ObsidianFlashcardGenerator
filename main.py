3bf01b0f0fd0441f6743050a34656a8062393a17
Hendrik <hendrik.wels@web.de>
2023-03-30 15:35:45 +0200

    Changed entire Code to more Modularity and Readability

--- main.py
+++ main.py
@@ -3,73 +3,70 @@ import re
 import openai
 
 
-def generate_questions_for_files_with_hashtags(api_key, folder_path):
-    # Authenticate with the OpenAI API using your API key
+def generate_questions_for_obsidian_files(api_key, folder_path):
+    """Main Function: Generates questions for all .md files in the specified folder
+    that contain both the #aws and #cloud hashtags and returns the new Folder Path"""
+
     openai.api_key = api_key
     prompt = "Füge eine Frage für den folgenden Text auf deutsch hinzu.\n"
 
-    # Create a new directory to store the modified files
-    new_folder_path = os.path.join(folder_path, 'AWS')
-    os.makedirs(new_folder_path, exist_ok=True)
+    new_folder_path = create_new_directory(folder_path)
+    obsidian_files = find_obsidian_files_with_tags(folder_path)
 
-    # Get a list of all .md files in the specified folder that contain both the #aws and #cloud hashtags
-    md_files = find_md_files_with_hashtags(folder_path)
-
-    # Loop through each file and generate questions using the OpenAI API
-    for file in md_files:
+    for file in obsidian_files:
         print(f'Generating questions for {file}...')
-        # Read the contents of the file
-        with open(os.path.join(folder_path, file), 'r') as f:
-            contents = f.read()
-
+        contents = read_file_contents(os.path.join(folder_path, file))
         paragraphs = contents.split('Break:')
 
-        questions_per_paragraph = []
-        for paragraph in paragraphs:
+        question_per_paragraph = process_paragraphs(prompt, paragraphs)
+        new_contents = ''.join(question_per_paragraph)
 
-            print(paragraph)
-            # Extract Images in the Paragraph
-            image_pattern = re.compile(r'!\[\[Pasted image .*?\]\]')
-            images = image_pattern.findall(paragraph)
+        write_modified_contents(new_folder_path, file, new_contents)
 
-            # Remove Images from the Paragraph
-            paragraph = image_pattern.sub('', paragraph)
-
-            questions = generate_questions(prompt + paragraph)
-            questions_per_paragraph.append('\nSTART\nBasic\n')
-            questions_per_paragraph.extend(questions.replace('?', '?\nBack:\n'))
-            questions_per_paragraph.extend(paragraph)
-
-            # Append the images after the Answer
-            for image in images:
-                questions_per_paragraph.append('\n' + image)
-
-            questions_per_paragraph.append('\nTags: #aws #cloud #solutions_architect #tech_fundamentals')
-            questions_per_paragraph.append('\nEND\n')
-
-        new_contents = ''.join(questions_per_paragraph)
-
-        # Write the modified contents to a new file in the new directory
-        with open(os.path.join(new_folder_path, file), 'w') as f:
-            f.write('TARGET DECK\nAWS SOLUTIONS ARCHITECT\n')
-            f.write(new_contents)
-
-    # Return the path to the new directory
     return new_folder_path
 
 
-def find_md_files_with_hashtags(folder_path):
+def create_new_directory(folder_path):
+    """Creates a new directory to store the modified files"""
+    new_folder_path = os.path.join(folder_path, 'AWS')
+    os.makedirs(new_folder_path, exist_ok=True)
+    return new_folder_path
+
+
+def read_file_contents(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
+
+
+def write_modified_contents(new_folder_path, file, new_contents):
+    with open(os.path.join(new_folder_path, file), 'w') as f:
+        f.write('TARGET DECK\nAWS SOLUTIONS ARCHITECT\n')
+        f.write(new_contents)
+
+
+def find_obsidian_files_with_tags(folder_path):
     md_files = [f for f in os.listdir(folder_path) if f.endswith('.md')]
     matching_files = []
     for file in md_files:
-        with open(os.path.join(folder_path, file), 'r') as f:
-            contents = f.read()
-            if re.search(r'#aws', contents, re.IGNORECASE) and re.search(r'#cloud', contents, re.IGNORECASE):
-                matching_files.append(file)
-    return matching_files
+        contents = read_file_contents(os.path.join(folder_path, file))
+        if re.search(r'#aws', contents, re.IGNORECASE) and re.search(r'#cloud', contents, re.IGNORECASE):
+            matching_files.append(file)
+    return matching_files[:1]
 
 
-def generate_questions(text):
+def process_paragraphs(prompt, paragraphs):
+    question_per_paragraph = []
+    for paragraph in paragraphs:
+        question = generate_question(prompt + paragraph)
+        question_per_paragraph.append('\nSTART\nBasic\n')
+        question_per_paragraph.extend(question.replace('?', '?\nBack:\n'))
+        question_per_paragraph.extend(paragraph)
+        question_per_paragraph.append('\nTags: #aws #cloud #solutions_architect #tech_fundamentals')
+        question_per_paragraph.append('\nEND\n')
+    return question_per_paragraph
+
+
+def generate_question(text):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0301",
         messages=[
@@ -80,7 +77,6 @@ def generate_questions(text):
     return reply_content
 
 
-
-api_key = open('api_key.txt', 'r').read().strip('\n')
+api_key = read_file_contents('api.txt').strip('\n')
 folder_path = '/Users/hendrikwels/Library/Mobile Documents/iCloud~md~obsidian/Documents/Book Notes'
-new_folder_path = generate_questions_for_files_with_hashtags(api_key, folder_path)
+new_folder_path = generate_questions_for_obsidian_files(api_key, folder_path)
